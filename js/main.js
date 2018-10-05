21d370ec0fe6530b6ca412290cfecf48c170de6b
Timur <timdino89@gmail.com>
2018-10-05 10:08:31 +0300

    Added Slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider-box').slick({
+    dots: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true
+  });
+});
