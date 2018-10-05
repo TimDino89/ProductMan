fcc9a162f066b161cb8a66336fb326398d2a6a54
Timur <timdino89@gmail.com>
2018-10-05 21:22:40 +0300

    Added popup window for subscribe form

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
   $('.slider-box').slick({
     dots: true,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
     arrows: false,
     accessebillity: true
   });
 
   $('.container').mixItUp();
 
+  $('.popup').magnificPopup();
 });
